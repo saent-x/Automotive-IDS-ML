31c59ca4032e4c33723498f53ec80a0b038b5b1a
saent-x <torsxperience@outlook.com>
2024-06-13 15:31:33 +0100

    Implementation of Random Forests

--- pkg/train/algo.go
+++ pkg/train/algo.go
@@ -1,13 +1,36 @@
 package train
 
-import "github.com/sjwhitworth/golearn/base"
+import (
+	"github.com/saent-x/go-ml-pl/pkg/core"
+	"github.com/sjwhitworth/golearn/base"
+	"github.com/sjwhitworth/golearn/ensemble"
+	"github.com/sjwhitworth/golearn/evaluation"
+	"golang.org/x/exp/rand"
+)
 
-func RandomForests() {
-	base.ParseCSVToInstances()
+func ImplRandomForests(cdi core.CleanedDatasetInfo) error {
+	// read the pre-processed dataset into golearn
+	// this part of the implementation is temp, TODO: to be replced with SplitFeatureTarget function
+	attackDS, err := base.ParseCSVToInstances(cdi.FileInfo.Filepath, true)
+	if err != nil {
+		return err
+	}
+
+	// set value to seed random proceses
+	rand.Seed(44111342) // TODO: revise seed value
+	// no of features per tree for rf set to sqroot of attackDS features
+	rf := ensemble.NewRandomForest(10, 3) // TODO: revise forestSize and features
+
+	// Use cross-fold validation to successively train and evaluate the model
+	// on 5 folds of the data set.
+	cv, err := evaluation.GenerateCrossFoldValidationConfusionMatrices(attackDS /* TODO: change [attackDS] to [xfeatures]*/, rf, 10)
+	if err != nil {
+		return err
+	}
 }
 
-func XGBoost() {
+func ImplXGBoost() {
 }
 
-func KMeansClustering() {
+func ImplKMeansClustering() {
 }
